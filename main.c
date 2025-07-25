06280747ef062aa385e8ec51b838277baf58c359
Khizar Ali <arslanmughallite@gmail.com>
2025-07-25 18:55:54 +0500

    added some lines

--- main.c
+++ main.c
@@ -2,5 +2,11 @@
 
 int main(void) {
     printf("myu name is khizarrrrr\n");
+    printf("myu name is khizarrrrr\n");
+    printf("myu name is khizarrrrr\n");
+    printf("myu name is khizarrrrr\n");
+    printf("myu name is khizarrrrr\n");
+    printf("myu name is khizarrrrr\n");
+    printf("myu name is khizarrrrr\n");
     return 0;
 }
