cc16bd57e31b117e20ab210629bceb12dc6808f5
Khizar Ali <arslanmughallite@gmail.com>
2025-07-25 19:07:30 +0500

    Printed a table of 5 using for loop

--- main.c
+++ main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("myu name is khizarrrrr\n");
-    printf("myu name is khizarrrrr\n");
-    printf("myu name is khizarrrrr\n");
-    printf("myu name is khizarrrrr\n");
-    printf("myu name is khizarrrrr\n");
-    printf("myu name is khizarrrrr\n");
-    printf("myu name is khizarrrrr\n");
+    int a = 5;
+    printf("Printing the table:\n");
+
+    for (int i = 1; i <= 100; i++) {
+        printf("%d x %d = %d\n", a, i, a * i);
+    }
+
+    printf("Hello world\n");
     return 0;
 }
